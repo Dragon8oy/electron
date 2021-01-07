15897ffb463cb9443baf4f77dd18ff8e25b85acb
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2021-01-07 19:55:17 +0000

    Removed MacOS specific code

--- main.js
+++ main.js
@@ -126,15 +126,3 @@ function sendMessage (content) {
 function forceLoad (content) {
   mainWindow.webContents.send('forceLoad', content);
 }
-
-app.on('window-all-closed', function () {
-  if(process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  if(mainWindow === null) {
-    createWindow()
-  }
-})
