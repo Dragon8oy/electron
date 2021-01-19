087e1330af049ea05f02b4ed2c807ecd825d5e32
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2021-01-19 21:42:39 +0000

    Fixed searching for strings in the text

--- assets/js/findText.js
+++ assets/js/findText.js
@@ -1,6 +1,8 @@
-var text
-function findText(text) {
-  if(window.find(text)) {
+function searchText() {
+  let searchBox = document.getElementById('searchBox');
+  //Highlight the text to be searched for, otherwise display an error message
+  //set aWrapAround to true
+  if(window.find(searchBox.value, false, false, true)) {
   } else {
     window.alert('No text found matching the search')
   }
